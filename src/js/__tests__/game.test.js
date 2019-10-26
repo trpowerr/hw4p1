5cb562529e208746ec9c9a31c7669d54374dcc44
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-10-26 16:44:09 +0300

    Update game.test.js

--- src/js/__tests__/game.test.js
+++ src/js/__tests__/game.test.js
@@ -3,39 +3,27 @@ import { healthPoint, unit } from '../game';
 let expected = 0;
 let received = 0;
 
-// test('should introduce correct healthpoint', () => {
-//   const received = healthPoint();
-//   if (unit.health > 50) {
-//     expected = 'healthy';
-//   } else if (unit.health < 15) {
-//     expected = 'critical';
-//   } else {
-//     expected = 'wounded';
-//   }
-
-//   expect(received).toBe(expected);
-// });
-
 test('should introduce correct healthpoint', () => {
-  received = healthPoint();
-  if (unit.health > 50) {
+  received = healthPoint(70);
+  if (unit.health >= 50) {
     expected = 'healthy';
     expect(received).toBe(expected);
   }
 });
 
 test('should introduce correct healthpoint', () => {
-  received = healthPoint();
-  if (unit.health < 15) {
+  received = healthPoint(10);
+  if (unit.health <= 15) {
     expected = 'critical';
     expect(received).toBe(expected);
   }
 });
 
 test('should introduce correct healthpoint', () => {
-  received = healthPoint();
-  if (unit.health <= 50 & unit.health >= 15) {
+  received = healthPoint(40);
+  if (unit.health < 50 & unit.health > 15) {
     expected = 'wounded';
     expect(received).toBe(expected);
   }
 });
+
